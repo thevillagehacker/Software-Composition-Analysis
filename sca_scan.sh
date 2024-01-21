694cc3f77181488f14b2c2a3bd02669e36164157
thevillagehacker <nvnj1998@gmail.com>
2024-01-21 21:17:25 +0530

    updated code

--- sca_scan.sh
+++ sca_scan.sh
@@ -7,30 +7,30 @@ mkdir SCA
 cd ~/SCA
 
 # Clean up existing setup
-echo "[+] Cleaning up..."
+echo -e "\e[92m[+]\e[0m Cleaning up..."
 sudo rm -r *
 
 # OWASP Dependency Check installation
 
 function owasp_dependency_check_setup {
 
-    echo "[*] Setting up OWASP Dependency check..."
+    echo -e "\e[94m[*]\e[0m Setting up OWASP Dependency check..."
     # Define the file and folder names
     file_name="dependency-check-9.0.9-release.zip"
     folder_name="dependency-check"
 
     # Check if the folder exists
     if [ -d "$folder_name" ]; then
-        echo "[*] $folder_name already exists. Skipping unzip."
+        echo -e "\e[94m[*]\e[0m $folder_name already exists. Skipping unzip."
     else
         # Check if the file exists
-        echo "[*] Downloading OWASP Dependency check Script..."
+        echo -e "\e[94m[*]\e[0m Downloading OWASP Dependency check Script..."
         wget -q --show-progress "https://github.com/jeremylong/DependencyCheck/releases/download/v9.0.9/dependency-check-9.0.9-release.zip"
         if [ -e "$file_name" ]; then
-            echo "[*] Unzipping OWASP Dependency check..."
+            echo -e "\e[94m[*]\e[0m Unzipping OWASP Dependency check..."
             unzip -q "$file_name"
         else
-            echo "[-] Error: $file_name not found."
+            echo -e "\e[91m[-]\e[0m Error: $file_name not found."
         fi
     fi
 }
@@ -53,15 +53,15 @@ function npm_audit {
 
 if command -v npm &> /dev/null; then
     npm_version=$(npm --version)
-    echo "[+] npm is already installed. Version: $npm_version"
+    echo -e "\e[92m[+]\e[0m npm is already installed. Version: $npm_version"
 else
-    echo "[-] npm not found. Installing npm..."
-    sudo apt-get update
-    sudo apt-get install npm
+    echo -e "\e[91m[-]\e[0m npm not found. Installing npm..."
+    sudo apt-get update &>/dev/null  # Suppressing stdout
+    sudo apt-get install npm -y &>/dev/null  # Suppressing stdout
 fi
 
-echo "[+] Initiating NPM audit..."
-echo "[+] Checking for node-modules target..."
+echo -e "\e[94m[*]\e[0m Initiating NPM audit..."
+echo -e "\e[94m[*]\e[0m Checking for node-modules target..."
 
 cd "$dir"
 
@@ -72,23 +72,23 @@ lock_file="package-lock.json"
 # Check if the folder exists
 if [ -d "$folder_name" ]; then
     cd "$folder_name"
-    echo "[+] Getting into $folder_name"
+    echo -e "\e[94m[*]\e[0m Getting into $folder_name"
     
     if [ -e "$lock_file" ]; then
-        echo "[+] $lock_file found in $folder_name"
+        echo -e "\e[92m[+]\e[0m $lock_file found in $folder_name"
         npm audit
     else
-        echo "[-] $lock_file not found in $folder_name"
+        echo -e "\e[91m[-]\e[0m $lock_file not found in $folder_name"
     fi       
 else
-    echo "[-] $folder_name not found"
-    echo "[+] Checking for $lock_file in the current working directory"
+    echo -e "\e[91m[-]\e[0m $folder_name not found"
+    echo -e "\e[94m[*]\e[0m Checking for $lock_file in the current working directory"
     
     if [ -e "$lock_file" ]; then
-        echo "[+] $lock_file found in the current working directory"
+        echo -e "\e[92m[+]\e[0m $lock_file found in the current working directory"
         npm audit
     else
-        echo "[-] $lock_file not found in the current working directory"
+        echo -e "\e[91m[-]\e[0m $lock_file not found in the current working directory"
     fi
 fi
 }
@@ -99,8 +99,8 @@ fi
 
 function snyk_run {
 
-echo "[+] Initiating Snyk audit..."
-echo "[+] Snyk setup started..."
+echo -e "\e[94m[*]\e[0m Initiating Snyk audit..."
+echo -e "\e[94m[*]\e[0m Snyk setup started..."
 
 cd ~/SCA
 wget -q --show-progress https://static.snyk.io/cli/latest/snyk-linux
@@ -109,12 +109,12 @@ sudo mv snyk-linux /usr/local/bin/snyk
 
 # Snyk configure
 
-echo "[+] Authenticate snyk... you will be redirected to a web page"
+echo -e "\e[94m[*]\e[0m Authenticate snyk... you will be redirected to a web page"
 if snyk auth; then
-    echo "[+] Authentication successful"
+    echo -e "\e[92m[+]\e[0m Authentication successful"
     cd "$dir"
 else
-    echo "[-] Authentication failed. Please check your credentials."
+    echo -e "\e[91m[-]\e[0m Authentication failed. Please check your credentials."
 fi
 
 # Run snyk
@@ -126,23 +126,23 @@ lock_file="package-lock.json"
 # Check if the folder exists
 if [ -d "$folder_name" ]; then
     cd "$folder_name"
-    echo "[+] Getting into $folder_name"
+    echo -e "\e[94m[*]\e[0m Getting into $folder_name"
     
     if [ -e "$lock_file" ]; then
-        echo "[+] $lock_file found in $folder_name"
+        echo -e "\e[92m[+]\e[0m $lock_file found in $folder_name"
         snyk test
     else
-        echo "[-] $lock_file not found in $folder_name"
+        echo -e "\e[91m[-]\e[0m $lock_file not found in $folder_name"
     fi       
 else
-    echo "[-] $folder_name not found"
-    echo "[+] Checking for $lock_file in the current working directory"
+    echo -e "\e[91m[-]\e[0m $folder_name not found"
+    echo -e "\e[94m[*]\e[0m Checking for $lock_file in the current working directory"
     
     if [ -e "$lock_file" ]; then
-        echo "[+] $lock_file found in the current working directory"
+        echo -e "\e[92m[+]\e[0m $lock_file found in the current working directory"
         snyk test
     else
-        echo "[-] $lock_file not found in the current working directory"
+        echo -e "\e[91m[-]\e[0m $lock_file not found in the current working directory"
     fi
 fi
 }
